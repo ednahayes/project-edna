f621c671297806e363ccc6a4814bbe149c0ad268
Edna Hayes <princessahe@hotmail.com>
2017-05-30 18:16:14 -0500

    checkpoint-4-new: not working

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,13 +1,22 @@
 (function() {
-    function RoomCtrl(Room) {
+    function RoomCtrl(Room, Message) {
       this.rooms = Room.all;
       this.createRoomForm = Room.add;
       this.room = null;
+      this.Message = {};
 
-}
+
+      this.openRoom = function(room) {
+      this.currentRoomTitle = room.name;
+      this.currentRoomMessages = Message.getByRoomId;
+
+        }
+
+
+      }
 
 
     angular
         .module('blocChat')
-        .controller('RoomCtrl', ['Room',  RoomCtrl]);
+        .controller('RoomCtrl', ['Room', RoomCtrl]);
 })();
